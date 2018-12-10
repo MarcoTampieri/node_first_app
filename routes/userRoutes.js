e7024f427d289e47c67d505186941210e11fa34b
Marco <mcg.tampieri@gmail.com>
2018-12-10 16:54:02 +0100

    lunedi pomeriggio

---
+++ routes/userRoutes.js
@@ -0,0 +1,38 @@
+const jsonfile = require("jsonfile");
+
+module.exports = app => {
+
+  app.get("/users", (req, res) => {
+    console.log("fetching all users");
+
+    // jsonfile reading
+    jsonfile.readFile("./DB/users.json", function (err, content) {
+      // send file contents back to sender
+      res.send(content);
+    });
+  });
+
+
+  app.post("/users/new", (req, res) => {
+
+    let email = req.body.email
+    let username = req.body.username
+
+    jsonfile.readFile("./DB/users.json", function (err, content) {
+
+      content.push({
+        email: email,
+        username: username
+      });
+
+      console.log("added " + email + "to DB");
+
+      jsonfile.writeFile("./DB/users.json", content, function (err) {
+        console.log(err);
+      });
+
+      res.sendStatus(200);
+    });
+  });
+
+};
