073797d71adfbd73fa69e001adf269fe1b91982f
Marco <mcg.tampieri@gmail.com>
2018-12-13 10:42:45 +0100

    first app done

--- routes/userRoutesEsercizio.js
+++ routes/userRoutesEsercizio.js
@@ -5,7 +5,7 @@ module.exports = app => {
 
     app.get("/users", (req, res) => {
         console.log("getting all exercise elements");
-        jsonfile.readFile(file_path, function(err, content) {
+        jsonfile.readFile(file_path, function (err, content) {
             res.send(content)
         })
     });
@@ -27,5 +27,49 @@ module.exports = app => {
 
             res.sendStatus(200)
         });
+    });
+
+    app.delete("/users", (req, res) => {
+        let pantheon = req.body.pantheon;
+        console.log(pantheon);
+        jsonfile.readFile(file_path, function (err, content) {
+
+            for (var i = content.length - 1; i >= 0; i--) {
+                if (content[i].pantheon === pantheon) {
+                    console.log("removing " + content[i].pantheon + "from DB");
+                    content.splice(i, 1);
+                }
+            }
+
+            jsonfile.writeFile(file_path, content, function (err) {
+                console.log(err);
+            });
+            res.sendStatus(200);
+        });
+    });
+
+    app.put("/users", (req, res) => {
+        let user;
+        let pantheon = req.query.pantheon;
+        let pantheonMod = req.body.pantheon;
+        let dei = req.body.dei;
+        let note = req.body.note;
+        console.log(pantheon);
+        jsonfile.readFile(file_path, function (err, content) {
+            for (i = content.length - 1; i >= 0; i--) {
+                if (content[i].pantheon === pantheon) {
+                    console.log("updated user " + pantheon + " has now gods : " + dei);
+                    user = content[i];
+                    user.pantheon = pantheonMod;
+                    user.dei = dei;
+                    user.note = note;
+                }
+            }
+
+            jsonfile.writeFile(file_path, content, function (err) {
+                console.log(err);
+            });
+        })
+        res.send(200);
     })
 }
